2c2692ee5694efd99a8f9eb8d005907963ad174d
Hamed Niroomand <hamed@niroomand.dev>
2026-09-07 04:08:21 +0330

    fix(sentry): disable tunnel configuration in Sentry setup

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -17,7 +17,7 @@ Sentry.init({
   environment,
   sendDefaultPii: false,
   tracesSampleRate: 0,
-  tunnel: '/tunnel',
+  // tunnel: '/tunnel',
   ignoreErrors: [
     'ResizeObserver loop',
     'Failed to fetch',
